8ee444849c541fe2fd292c688f75cfeae9a0817e
Rohit <you@example.com>
2024-05-13 10:10:55 -0500

    Adding more code

--- news_automation.py
+++ news_automation.py
@@ -1,3 +1,31 @@
+import sys
+
 API_KEY = 'd25cc0bde2cd4aeca732c66175bcc2d9'
 
 URL = ('https://newsapi.org/v2/top-headlines?')
+
+def get_articles_by_category(category):
+    query_parameters = {
+        "category": category,
+        "sortBy": "top",
+        "country": "us",
+        "apiKey": API_KEY
+    }
+    return _get_articles(query_parameters)
+
+def _get_articles(params):
+    response = requests.get(URL, params=params)
+
+    articles = response.json()['articles']
+
+    results = []
+
+    for article in articles:
+        results.append({"title": article["title"], "url": article["url"]})
+
+    for result in results:
+        print(result['title'])
+        print(result['url'])
+        print('')
+
+
